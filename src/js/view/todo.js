fc1108a5d89038d83d9be07df8a812c40b813831
macqueen <jessmacqueen@gmail.com>
2014-02-12 14:37:44 -0800

    adds ability to delete tasks

--- src/js/view/todo.js
+++ src/js/view/todo.js
@@ -4,7 +4,11 @@ var TodoView = Backbone.View.extend({
 
   className: 'todo-item',
 
-  template: _.template('<input type="checkbox"><%= task %></input>'),
+  events: {
+    'click .delete': 'deleteTodo'
+  },
+
+  template: _.template('<input type="checkbox"><%= task %></input><span class="delete">X</span>'),
 
   initialize: function(model) {
     this.model = model;
@@ -13,6 +17,11 @@ var TodoView = Backbone.View.extend({
 
   render: function() {
     this.$el.html(this.template(this.model.toJSON()));
+  },
+
+  deleteTodo: function() {
+    this.model.collection.remove(this.model);
+    this.remove();
   }
 
 });
